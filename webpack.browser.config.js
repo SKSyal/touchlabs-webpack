4ca9af6643e31a4e6ada48df6b5082cb9d104fc4
taraojo <tara.ojo@gmail.com>
2016-09-09 14:40:33 +0100

    add eslint

--- webpack.browser.config.js
+++ webpack.browser.config.js
@@ -1,25 +1,24 @@
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var path = require('path');
 
 module.exports = {
     entry: {
-      searchPage: "./src/searchPage.js",
-      landingPage: "./src/landingPage.js"
+        searchPage: './src/searchPage.js', landingPage: './src/landingPage.js'
     },
     output: {
         path: path.resolve(__dirname, 'assets'),
-        filename: "./js/[name].js",
-        chunkFilename: "[id].js"
+        filename: './js/[name].js',
+        chunkFilename: '[id].js'
     },
     module: {
         loaders: [
             {
-              test: /\.scss$/,
-              loader: ExtractTextPlugin.extract("style-loader", "css-loader")
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
             }
         ]
     },
     plugins: [
-        new ExtractTextPlugin("./css/[name].css")
+        new ExtractTextPlugin('./css/[name].css')
     ]
 };

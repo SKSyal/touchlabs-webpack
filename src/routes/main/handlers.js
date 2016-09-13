9b725de6b40458d49a3049dc0f127d22c8daf7b0
Sunil <skumar225@sapient.com>
2016-09-13 16:48:48 +0100

    added handlebars client,server side

---
+++ src/routes/main/handlers.js
@@ -0,0 +1,13 @@
+
+var index = function(req, res) {
+    res.render('plp', {
+        head: { title: 'Main Page Title' },
+        weather: {
+          location: 'London'
+        }
+    });
+}
+
+module.exports = {
+    index: index
+};
